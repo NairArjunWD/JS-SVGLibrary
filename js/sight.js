6e0030a89e97f004ce4d074f34613c65dc5a5d80
Arjun Nair <nair.arjun47@yahoo.com>
2021-07-15 19:55:23 -0700

    Fix index Error
    Fix index error by replacing the = with :

--- js/sight.js
+++ js/sight.js
@@ -17,8 +17,7 @@ class SVGElement {
 
     append(element) {
 
-        const parent = (typeof element === 'string') ? 
-            document.querySelector(element) : element.node;
+        const parent = (typeof element === 'string') ? document.querySelector(element) : element.node;
         
         parent.appendChild(this.node);
 
